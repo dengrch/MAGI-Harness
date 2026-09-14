d89d6a002336a1b1fd662fd016eb136a5f6d0874
dengrch <dengrc233@gmail.com>
2026-09-14 18:12:06 +0800

    tmp close version after cleaning

--- build/lib/mgh/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Small, directly embeddable context runtime."""
-
-from .context import ContextCompiler, ContextConfig
-from .runtime import Harness
-from .store import Store
-
-__all__ = ["ContextCompiler", "ContextConfig", "Harness", "Store"]
